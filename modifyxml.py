6c140ef8a6b0bb0dfa6b92c659ca5cd7208d43e4
dan zheng <danz@email.com>
2020-07-03 00:13:20 +1000

    little changes

--- modifyxml.py
+++ modifyxml.py
@@ -1,4 +1,4 @@
-from lxml import etree as ElementTree
+from lxml import etree
 import os
 import re
 # from xml.etree import ElementTree
@@ -6,28 +6,27 @@ import re
 filename = "test.pzfx"
 full_file = os.path.abspath(os.path.join(".", filename))
 
-tree = ElementTree.parse(full_file)
+tree = etree.parse(full_file)
 root = tree.getroot()
-ns = re.match(r'{.*}', root.tag).group(0)
-table = root.find(f"{ns}Table")
-title = table.find(f"{ns}Title")
+# ns = re.match(r'{.*}', root.tag).group(0)
+# table = root.find(f"{ns}Table")
+# title = table.find(f"{ns}Title")
+ns = {"": "http://graphpad.com/prism/Prism.htm"}
+table = root.find("Table", namespaces=ns)
+title = table.find("Title", namespaces=ns)
+compd_titles = table.findall("YColumn/Title", namespaces=ns)
 
-compd_titles = table.findall(f"{ns}YColumn/{ns}Title")
 for t in compd_titles:
     print(t.text)
 
-data = table.findall(f"{ns}YColumn/{ns}Subcolumn/{ns}d")
-i = 20
+data = table.findall("YColumn/Subcolumn/d", namespaces=ns)
+i = 18
 for d in data:
     d.text = str(i)
     i += 1
     print(d.text)
 
-with open("new.pzfx", "w") as new_file:
-    new_file.write(r'<?xml version="1.0" encoding="UTF-8"?>')
-
-tree.write(open("new.pzfx", "ab"), encoding="UTF-8")
-
-
-# xml_string = r'<?xml version="1.0" encoding="UTF-8"?>' + ElementTree.tostring(root).decode()
-#
+new_file_name = f"{title.text}.pzfx"
+with open(new_file_name, "w") as f:
+    f.write('<?xml version="1.0" encoding="UTF-8"?>')
+tree.write(open(new_file_name, "ab"), encoding="UTF-8")
