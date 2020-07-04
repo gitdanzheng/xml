d8b177c24a78298d713675e4eec47c45189ac711
danz <66901678+gitdanzheng@users.noreply.github.com>
2020-07-05 02:11:40 +1000

    Update modifyxml.py

--- modifyxml.py
+++ modifyxml.py
@@ -1,32 +1,82 @@
 from lxml import etree
+import pandas as pd
 import os
-import re
 # from xml.etree import ElementTree
 
-filename = "test.pzfx"
-full_file = os.path.abspath(os.path.join(".", filename))
 
-tree = etree.parse(full_file)
-root = tree.getroot()
-# ns = re.match(r'{.*}', root.tag).group(0)
-# table = root.find(f"{ns}Table")
-# title = table.find(f"{ns}Title")
-ns = {"": "http://graphpad.com/prism/Prism.htm"}
-table = root.find("Table", namespaces=ns)
-title = table.find("Title", namespaces=ns)
-compd_titles = table.findall("YColumn/Title", namespaces=ns)
+def plasma_pzfx_to_df(pzfx=r"C:\Users\danz\github\xml\plasma_template.pzfx"):
+    # """
+    # define a function to parse pzfx file (plasma result) into a pd dataframe
+    #
+    # """
 
-for t in compd_titles:
-    print(t.text)
+    tree = etree.parse(pzfx)
+    root = tree.getroot()
+    ns = {"": "http://graphpad.com/prism/Prism.htm"}
+    table = root.find("Table", namespaces=ns)
+    title = table.find("Title", namespaces=ns)
+    prodrug = title.text
 
-data = table.findall("YColumn/Subcolumn/d", namespaces=ns)
-i = 18
-for d in data:
-    d.text = str(i)
-    i += 1
-    print(d.text)
+    compd_titles = table.findall("YColumn/Title", namespaces=ns)
+    compd_title_list = [t.text for t in compd_titles]
+    columns = []
+    for compd_title in compd_title_list:
+        for repeat in range(1, 4):
+            columns.append(compd_title + str(repeat))
 
-new_file_name = f"{title.text}.pzfx"
-with open(new_file_name, "w") as f:
-    f.write('<?xml version="1.0" encoding="UTF-8"?>')
-tree.write(open(new_file_name, "ab"), encoding="UTF-8")
+    x_data = table.findall("XColumn/Subcolumn/d", namespaces=ns)
+    x_data_list = [d.text for d in x_data]
+
+    y_data = table.findall("YColumn/Subcolumn/d", namespaces=ns)
+    y_data_list = [d.text for d in y_data]
+
+    length = len(x_data_list)
+    # split y_data_list into 9 small lists
+    y_lists = [y_data_list[i: i + length] for i in range(0, len(y_data_list), length)]
+
+    df = pd.DataFrame(y_lists, columns=x_data_list)
+    df = df.T
+    df.columns = columns
+    return title, df
+
+
+def df_to_pzfx(prodrug, df):
+    templete_pzfx = r"C:\Users\danz\github\xml\plasma_template.pzfx"
+    tree = etree.parse(templete_pzfx)
+    root = tree.getroot()
+    ns = {"": "http://graphpad.com/prism/Prism.htm"}
+    table = root.find("Table", namespaces=ns)
+    title = table.find("Title", namespaces=ns)
+    title.text = prodrug
+    info_title = table.find("Info/Title", namespaces=ns)
+    info_title.text = prodrug
+
+    data_list = []
+    for column in df.items():
+        for i in column[1]:
+            data_list.append(i)
+
+    subcolumns = table.findall("YColumn/Subcolumn", namespaces=ns)
+    j = 0
+    for subcolumn in subcolumns:
+        data = subcolumn.findall("d", namespaces=ns)
+        for number in data:
+            number.text = data_list[j]
+            j += 1
+
+    new_file_name = f"{prodrug}.pzfx"
+    with open(new_file_name, "w") as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?>')
+    tree.write(open(new_file_name, "ab"), encoding="UTF-8")
+
+
+def main():
+    prodrug, df = plasma_pzfx_to_df()
+
+    df2 = df+"test2"
+
+    df_to_pzfx("test_prodrug", df2)
+
+
+if __name__ == "__main__":
+    main()
